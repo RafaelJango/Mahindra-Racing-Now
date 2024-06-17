7910bcd79de80c4ca6184ec4c99f1cb341711553
Jango <rafaeljango@protonmail.com>
2024-06-17 01:55:28 -0300

    Mudança JS

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// slideshow script
+// slideshow
 
 let slideIndex = 0;
 showSlides();
@@ -66,6 +66,6 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); 
 }
 
